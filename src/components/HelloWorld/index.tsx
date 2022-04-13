a081c4941dfc75f3637b123f9cc784e0bce7036f
balda-kamil <baldakamil@gmail.com>
2022-04-13 09:12:32 +0200

    axios added

--- src/components/HelloWorld/index.tsx
+++ src/components/HelloWorld/index.tsx
@@ -1,22 +1,29 @@
-import React from "react";
+import React, {useEffect} from "react";
+import axios from "axios";
 
-const HelloWorld: React.FC = () => (
-    <>
-        <h1>Hello World</h1>
+const HelloWorld: React.FC = () => {
+    useEffect(() => {
+        axios.get("http://localhost/users/role/administrator").then((res) => console.log(res));
+    }, []);
 
-        <hr />
+    return (
+        <>
+            <h1>Hello World</h1>
 
-        <h3>Environmental variables:</h3>
-        <p>
-            process.env.PRODUCTION: <b>{process.env.PRODUCTION.toString()}</b>
-        </p>
-        <p>
-            process.env.NAME: <b>{process.env.NAME}</b>
-        </p>
-        <p>
-            process.env.VERSION: <b>{process.env.VERSION}</b>
-        </p>
-    </>
-);
+            <hr />
+
+            <h3>Environmental variables:</h3>
+            <p>
+                process.env.PRODUCTION: <b>{process.env.PRODUCTION.toString()}</b>
+            </p>
+            <p>
+                process.env.NAME: <b>{process.env.NAME}</b>
+            </p>
+            <p>
+                process.env.VERSION: <b>{process.env.VERSION}</b>
+            </p>
+        </>
+    );
+};
 
 export default HelloWorld;
