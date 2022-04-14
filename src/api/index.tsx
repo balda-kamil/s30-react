68fe4a50ace3bc8ba6ec5ce5e51aae521cbe2e86
balda-kamil <baldakamil@gmail.com>
2022-04-14 10:59:06 +0200

    handling errors

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import {Inputs} from "components/Form";
+import {UserDataInterface} from "components/UsersPage";
 
 export const createUser = (formData: Inputs) => {
     return new Promise((resolve, reject) => {
@@ -13,37 +14,24 @@ export const createUser = (formData: Inputs) => {
                 resolve(response);
             })
             .catch(function (error) {
+                console.error(error);
                 reject(error);
             });
     });
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const fetchAllUsersByRoles = (roles: string[]): Promise<any[]> => {
-    return new Promise((res, rej) => {
-        Promise.all(
-            roles.map(
-                (role) =>
-                    new Promise((resolve, reject) => {
-                        axios
-                            .get(`${process.env.GET_USERS_BY_ROLE}/${role}`)
-                            .then(function (response) {
-                                const {data} = response;
-                                resolve({role, data});
-                            })
-                            .catch(function (error) {
-                                console.log(error);
-                                reject(error);
-                            });
-                    })
-            )
-        )
-            .then(function (resp) {
-                res(resp);
+export const fetchAllUsersByRoles = (role: string): Promise<UserDataInterface[]> => {
+    if (role === "") return;
+    return new Promise((resolve, reject) => {
+        axios
+            .get(`${process.env.GET_USERS_BY_ROLE}/${role}`)
+            .then(function (response) {
+                resolve(response.data);
             })
             .catch(function (error) {
-                console.error("ERROR", error);
-                rej(error);
+                console.error(error);
+                reject(error);
             });
     });
 };
--- src/components/UsersPage/index.tsx
+++ src/components/UsersPage/index.tsx
@@ -1,37 +1,89 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useEffect, useState} from "react";
 import {userRoles} from "components/UsersPage/userRoles";
 import {fetchAllUsersByRoles} from "src/api";
-import {Button} from "@mui/material";
+import Select, {SelectChangeEvent} from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import {
+    Grid,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from "@mui/material";
+import Paper from "@mui/material/Paper";
 
 export interface UserDataInterface {
-    role: string;
-    data: {
-        users: {
-            id: number;
-            name: string;
-            email: string;
-        }[];
-    };
+    objArr: {
+        role: string;
+        data: {
+            users: {
+                id: number;
+                name: string;
+                email: string;
+            }[];
+        };
+    }[];
 }
 
 const UsersPage: React.FC = () => {
     const [usersData, setUsersData] = useState<UserDataInterface[]>([]);
 
-    const fetchData = async () => {
-        const response = await fetchAllUsersByRoles(userRoles);
+    const fetchData = async (role) => {
+        const response = await fetchAllUsersByRoles(role);
+        console.log("response", response);
         setUsersData(response);
     };
 
     useEffect(() => {
-        fetchData();
-    }, []);
+        console.log("usersData", usersData);
+    }, [usersData]);
+
+    const handleChange = (event: SelectChangeEvent) => {
+        fetchData(event.target.value);
+    };
 
     return (
         <>
-            {usersData &&
-                usersData.map((item, i) => {
-                    return <Button key={i}>{item.role}</Button>;
-                })}
+            <Grid container sx={{mb: 4}}>
+                <FormControl fullWidth sx={{mb: 3}}>
+                    <InputLabel id="users-sole-select">Roles</InputLabel>
+
+                    <Select
+                        id="users-sole-select"
+                        label="Roles"
+                        onChange={handleChange}
+                        defaultValue=""
+                    >
+                        {userRoles.map((item, i) => {
+                            return (
+                                <MenuItem key={i} value={item}>
+                                    {item}
+                                </MenuItem>
+                            );
+                        })}
+                    </Select>
+                </FormControl>
+
+                {usersData.length > 0 && (
+                    <TableContainer component={Paper}>
+                        <Table aria-label="users table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>User ID</TableCell>
+                                    <TableCell align="right">Email</TableCell>
+                                    <TableCell align="right">Name</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody></TableBody>
+                        </Table>
+                    </TableContainer>
+                )}
+            </Grid>
         </>
     );
 };
